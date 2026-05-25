1fa6c3c24d60231deb7fc1edf8f470fa60123939
AndresHuilinir <134530197+AndresHuilinir@users.noreply.github.com>
2026-05-24 23:04:03 -0400

    Delete Lanzar.bat

--- Lanzar.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\andre\IdeaProjects\Proyecto_Yog_Sothoth\Motor"
-"C:\Users\andre\AppData\Local\Programs\Python\Python314\python.exe" "C:\Users\andre\IdeaProjects\Proyecto_Yog_Sothoth\Motor\Ritual.py"
-pause
