6ba7d7f5071b8e86061105ed032529ce6e999840
Pulpo_Maldito <andres.huilinir@usm.cl>
2026-07-08 19:26:10 -0400

    Cambios

--- Lanzar.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\andre\Documents\GitHub\Proyecto_Yog_Sothoth\Motor"
-"C:\Users\andre\AppData\Local\Python\pythoncore-3.14-64\python.exe" "C:\Users\andre\Documents\GitHub\Proyecto_Yog_Sothoth\Motor\Ritual.py"
-pause
